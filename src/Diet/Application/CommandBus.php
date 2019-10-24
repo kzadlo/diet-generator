0b1c8923274a365ba3fa658e1defe5c322361a39
Krystian Żądło <krystianzadlo@fejm.pl>
2019-10-24 20:14:09 +0200

    Creating Command Bus

---
+++ src/Diet/Application/CommandBus.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Diet\Application;
+
+use App\Diet\Application\Exception\CommandHandlerNotFoundException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CommandBus
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function dispatch(object $command)
+    {
+        $handlerName = get_class($command) . 'Handler';
+
+        if (!$this->container->has($handlerName)) {
+            throw new CommandHandlerNotFoundException($handlerName . ' does not exist!');
+        }
+
+        $handler = $this->container->get($handlerName);
+        $handler->handle($command);
+    }
+}
